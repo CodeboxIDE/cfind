a8d3a011558bd8dec9727eedc3c89999d2b2b6bf
Samy Pessé <samypesse@gmail.com>
2014-07-25 21:18:50 -0700

    Add method to search for files

--- README.md
+++ README.md
@@ -13,11 +13,26 @@ npm install cfind
 
 ### Examples
 
+Search code:
+
 ```js
 var cfind = require("cfind");
 
 cfind.find({
     query: "jQuery",
+    root: "/Users/test/myworkspace"
+}, function(err, result) {
+
+});
+```
+
+Search files:
+
+```js
+var cfind = require("cfind");
+
+cfind.files({
+    query: "test.js",
     root: "/Users/test/myworkspace"
 }, function(err, result) {
 
---
+++ test/files.js
@@ -0,0 +1,15 @@
+var path = require("path");
+var cfind = require("../lib");
+
+describe('Find files', function() {
+    it('can search', function(done) {
+        cfind.files({
+            query: "tes",
+            root: path.join(__dirname, "fixtures")
+        }, function(err, result) {
+            if (err) return done(err);
+
+            done(result.matches == 1? null : new Error(""));
+        });
+    });
+});
